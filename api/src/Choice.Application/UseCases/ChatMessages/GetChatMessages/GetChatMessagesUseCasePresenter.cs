70fb27e00028710a99985886655163801392b4c0
sfgserr <dead01r@gmail.com>
2023-10-05 10:20:24 +0300

    Add GetChatMessagesUseCase

---
+++ api/src/Choice.Application/UseCases/ChatMessages/GetChatMessages/GetChatMessagesUseCasePresenter.cs
@@ -0,0 +1,14 @@
+﻿using Choice.Domain.Models;
+
+namespace Choice.Application.UseCases.ChatMessages.GetChatMessages
+{
+    public class GetChatMessagesUseCasePresenter : IOutputPort
+    {
+        public IList<ChatMessage> Chat { get; set; }
+
+        public void Ok(IList<ChatMessage> chat)
+        {
+            Chat = chat;
+        }
+    }
+}
---
+++ api/src/Choice.Application/UseCases/ChatMessages/GetChatMessages/IGetChatMessagesUseCase.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Choice.Application.UseCases.ChatMessages.GetChatMessages
+{
+    public interface IGetChatMessagesUseCase
+    {
+        Task Execute(int user1Id, int user2Id);
+
+        void SetOutputPort(IOutputPort outputPort);
+    }
+}
