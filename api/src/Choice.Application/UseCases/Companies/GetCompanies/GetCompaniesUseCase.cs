8e98ea759a994cf310528de3d9304bc1b9bf7f7e
sfgserr <dead01r@gmail.com>
2023-10-05 21:37:00 +0300

    Add GetCompaniesUseCase

---
+++ api/src/Choice.Application/UseCases/Companies/GetCompanies/GetCompaniesUseCase.cs
@@ -0,0 +1,34 @@
+﻿using Choice.Domain;
+using Choice.Domain.Models;
+
+namespace Choice.Application.UseCases.Companies.GetCompanies
+{
+    public class GetCompaniesUseCase : IGetCompaniesUseCase
+    {
+        private readonly IRepository<Company> _companyRepository;
+
+        private IOutputPort _outputPort;
+
+        public GetCompaniesUseCase(IRepository<Company> companyRepository)
+        {
+            _companyRepository = companyRepository;
+
+            _outputPort = new GetCompaniesUseCasePresenter();
+        }
+
+        public async Task Execute() =>
+            await GetCompanies();
+
+        private async Task GetCompanies()
+        {
+            IList<Company> companies = await _companyRepository.Get();
+
+            _outputPort.Ok(companies);
+        }
+
+        public void SetOutputPort(IOutputPort outputPort)
+        {
+            _outputPort = outputPort;
+        }
+    }
+}
---
+++ api/src/Choice.Application/UseCases/Companies/GetCompanies/GetCompaniesUseCasePresenter.cs
@@ -0,0 +1,14 @@
+﻿using Choice.Domain.Models;
+
+namespace Choice.Application.UseCases.Companies.GetCompanies
+{
+    public class GetCompaniesUseCasePresenter : IOutputPort
+    {
+        public IList<Company> Companies { get; set; }
+
+        public void Ok(IList<Company> companies)
+        {
+            Companies = companies;
+        }
+    }
+}
