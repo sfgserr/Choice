b12398507c3863586b4e85d363fdca97690e4fcd
sfgserr <dead01r@gmail.com>
2023-10-08 20:33:28 +0300

    Add ChatMessagesUseCases to API layer

---
+++ api/src/Choice.WebApi/UseCases/ChatMessages/GetChatMessages/ChatMessageController.cs
@@ -0,0 +1,35 @@
+﻿using Choice.Application.UseCases.ChatMessages.GetChatMessages;
+using Choice.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Choice.WebApi.UseCases.ChatMessages.GetChatMessages
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatMessageController : Controller, IOutputPort
+    {
+        private readonly IGetChatMessagesUseCase _useCase;
+
+        private IActionResult _viewModel;
+
+        public ChatMessageController(IGetChatMessagesUseCase useCase)
+        {
+            _useCase = useCase;
+        }
+
+        void IOutputPort.Ok(IList<ChatMessage> chat)
+        {
+            _viewModel = Ok(chat);
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get(int user1Id, int user2Id)
+        {
+            _useCase.SetOutputPort(this);
+
+            await _useCase.Execute(user1Id, user2Id);
+
+            return _viewModel;
+        }
+    }
+}
---
+++ api/src/Choice.WebApi/UseCases/ChatMessages/SendChatMessage/ChatMessageController.cs
@@ -0,0 +1,40 @@
+﻿using Choice.Application.UseCases.Messages.SendChatMessage;
+using Choice.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Choice.WebApi.UseCases.ChatMessages.SendChatMessage
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatMessageController : Controller, IOutputPort
+    {
+        private readonly ISendChatMessageUseCase _useCase;
+
+        private IActionResult _viewModel;
+
+        public ChatMessageController(ISendChatMessageUseCase useCase)
+        {
+            _useCase = useCase;
+        }
+
+        void IOutputPort.Ok(ChatMessage chatMessage)
+        {
+            _viewModel = Ok(chatMessage);
+        }
+
+        void IOutputPort.Invalid()
+        {
+            _viewModel = BadRequest();
+        }
+
+        [HttpPost("Send")]
+        public async Task<IActionResult> SendChatMessage(User sender, Room room, string text)
+        {
+            _useCase.SetOutputPort(this);
+
+            await _useCase.Execute(sender, room, text);
+
+            return _viewModel;
+        }
+    }
+}
