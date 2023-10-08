779fe50ab78bf0652ac4509b2e8fbb09697c0336
sfgserr <dead01r@gmail.com>
2023-10-08 14:52:19 +0300

    Add CreateCategoryUseCase

---
+++ api/src/Choice.Application/UseCases/Categories/CreateCategory/ICreateCategoryUseCase.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Choice.Application.UseCases.Categories.CreateCategory
+{
+    public interface ICreateCategoryUseCase
+    {
+        Task Execute(string title, string iconUri);
+
+        void SetOutputPort(IOutputPort outputPort);
+    }
+}
---
+++ api/src/Choice.Application/UseCases/Categories/CreateCategory/IOutputPort.cs
@@ -0,0 +1,11 @@
+﻿using Choice.Domain.Models;
+
+namespace Choice.Application.UseCases.Categories.CreateCategory
+{
+    public interface IOutputPort
+    {
+        void Ok(Category category);
+
+        void Invalid();
+    }
+}
