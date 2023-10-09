f85baf252f7740d58bc2e86536379971c98798d5
sfgserr <dead01r@gmail.com>
2023-10-09 19:37:58 +0300

    Add GetClientsUseCase to API layer

---
+++ api/src/Choice.WebApi/UseCases/Clients/GetClients/ClientController.cs
@@ -0,0 +1,35 @@
+﻿using Choice.Application.UseCases.Clients.GetClients;
+using Choice.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Choice.WebApi.UseCases.Clients.GetClients
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : Controller, IOutputPort
+    {
+        private readonly IGetClientsUseCase _useCase;
+
+        private IActionResult _viewModel;
+
+        public ClientController(IGetClientsUseCase useCase)
+        {
+            _useCase = useCase;
+        }
+
+        void IOutputPort.Ok(IList<Client> clients)
+        {
+            _viewModel = Ok(clients);
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get()
+        {
+            _useCase.SetOutputPort(this);
+
+            await _useCase.Execute();
+
+            return _viewModel;
+        }
+    }
+}
