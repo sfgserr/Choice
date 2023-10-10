278bdbc5f9645ce9f0169a7975461d7684ebd9de
sfgserr <dead01r@gmail.com>
2023-10-10 16:00:18 +0300

    Add UpdateOrderUseCase to API layer

---
+++ api/src/Choice.WebApi/UseCases/Orders/UpdateOrder/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Choice.Application.UseCases.Orders.UpdateOrder;
+using Choice.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Choice.WebApi.UseCases.Orders.UpdateOrder
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller, IOutputPort
+    {
+        private readonly IUpdateOrderUseCase _useCase;
+
+        private IActionResult _viewModel;
+
+        public OrderController(IUpdateOrderUseCase useCase)
+        {
+            _useCase = useCase;
+        }
+
+        void IOutputPort.Ok(Order order)
+        {
+            _viewModel = Ok(order);
+        }
+
+        [Route("Update")]
+        public async Task<IActionResult> Update(Order order)
+        {
+            _useCase.SetOutputPort(this);
+
+            await _useCase.Execute(order);
+
+            return _viewModel;
+        }
+    }
+}
