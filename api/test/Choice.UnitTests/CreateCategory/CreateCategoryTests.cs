73e987e6bd59a3088573d869842b7db63748243b
sfgserr <dead01r@gmail.com>
2023-10-16 19:41:48 +0300

    Add CreateCategory_Returns_Ok test

--- api/test/Choice.UnitTests/CreateCategory/CreateCategoryTests.cs
+++ api/test/Choice.UnitTests/CreateCategory/CreateCategoryTests.cs
@@ -1,7 +1,29 @@
-﻿
+﻿using Choice.Application.UseCases.Categories.CreateCategory;
+using Xunit;
+
 namespace Choice.UnitTests.CreateCategory
 {
-    public sealed class CreateCategoryTests
+    public sealed class CreateCategoryTests : IClassFixture<StandardFixture>
     {
+        private readonly StandardFixture _fixture;
+
+        public CreateCategoryTests(StandardFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Theory]
+        public async Task CreateCategory_Returns_Ok()
+        {
+            CreateCategoryPresenter presenter = new CreateCategoryPresenter();
+
+            CreateCategoryUseCase sut = new CreateCategoryUseCase(_fixture.CategoryRepositoryFake, _fixture.UnitOfWorkFake);
+
+            sut.SetOutputPort(presenter);
+
+            await sut.Execute("Category", "/photo");
+
+            Assert.NotNull(presenter.Category);
+        }
     }
 }
--- api/test/Choice.UnitTests/StandardFixture.cs
+++ api/test/Choice.UnitTests/StandardFixture.cs
@@ -1,7 +1,21 @@
-﻿
+﻿using Choice.Infrastructure;
+using Choice.Infrastructure.Repositories.Fakes;
+
 namespace Choice.UnitTests
 {
     public sealed class StandardFixture
     {
+        public StandardFixture() 
+        {
+            Context = new ChoiceContextFake();
+
+            CategoryRepositoryFake = new CategoryRepositoryFake(Context);
+
+            UnitOfWorkFake = new UnitOfWorkFake();
+        }
+
+        public CategoryRepositoryFake CategoryRepositoryFake { get; }
+        public ChoiceContextFake Context { get; }
+        public UnitOfWorkFake UnitOfWorkFake { get; }
     }
 }
