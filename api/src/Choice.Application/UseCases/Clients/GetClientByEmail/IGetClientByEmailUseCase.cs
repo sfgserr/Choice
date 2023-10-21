a2e215eb1838fc971f8dd77df9425d9d2e8cf00f
sfgserr <dead01r@gmail.com>
2023-10-21 10:22:29 +0300

    Add GetClientByEmailUseCase

---
+++ api/src/Choice.Application/UseCases/Clients/GetClientByEmail/IGetClientByEmailUseCase.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Choice.Application.UseCases.Clients.GetClientByEmail
+{
+    public interface IGetClientByEmailUseCase
+    {
+        Task Execute(string email);
+
+        void SetOutputPort(IOutputPort outputPort);
+    }
+}
