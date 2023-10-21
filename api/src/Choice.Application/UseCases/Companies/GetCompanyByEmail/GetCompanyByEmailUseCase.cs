a4d394439663e018ec929a17608fe568dc8d96d1
sfgserr <dead01r@gmail.com>
2023-10-21 10:30:33 +0300

    Add GetCompanyByEmailUseCase

---
+++ api/src/Choice.Application/UseCases/Companies/GetCompanyByEmail/GetCompanyByEmailUseCase.cs
@@ -0,0 +1,37 @@
+﻿using Choice.Domain;
+using Choice.Domain.Models;
+
+namespace Choice.Application.UseCases.Companies.GetCompanyByEmail
+{
+    public class GetCompanyByEmailUseCase : IGetCompanyByEmailUseCase
+    {
+        private readonly IRepository<Company> _companyRepository;
+
+        private IOutputPort _outputPort;
+
+        public GetCompanyByEmailUseCase(IRepository<Company> companyRepository)
+        {
+            _companyRepository = companyRepository;
+
+            _outputPort = new GetCompanyByEmailPresenter();
+        }
+
+        public async Task Execute(string phoneNumber)
+        {
+            Company company = await _companyRepository.GetBy(c => c.PhoneNumber == phoneNumber);
+
+            if (company != null)
+            {
+                _outputPort.Ok(company);
+                return;
+            }
+
+            _outputPort.NotFound();
+        }
+
+        public void SetOutputPort(IOutputPort outputPort)
+        {
+            _outputPort = outputPort;
+        }
+    }
+}
---
+++ api/src/Choice.Application/UseCases/Companies/GetCompanyByEmail/IGetCompanyByEmailUseCase.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Choice.Application.UseCases.Companies.GetCompanyByEmail
+{
+    public interface IGetCompanyByEmailUseCase
+    {
+        Task Execute(string phoneNumber);
+
+        void SetOutputPort(IOutputPort outputPort);
+    }
+}
