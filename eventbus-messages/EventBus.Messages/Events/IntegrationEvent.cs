2f8bc02bdb91dd9cec40474462ca784dfb4038ad
sfgserr <dead01r@gmail.com>
2024-02-09 20:04:13 +0200

    Add EventBus.Messages

---
+++ eventbus-messages/EventBus.Messages/Events/IntegrationEvent.cs
@@ -0,0 +1,22 @@
+﻿
+namespace EventBus.Messages.Events
+{
+    public class IntegrationEvent
+    {
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.Now;
+        }
+
+        public IntegrationEvent(Guid id, DateTime creationDate)
+        {
+            Id = id;
+            CreationDate = creationDate;
+        }
+
+        public Guid Id { get; private set; }
+
+        public DateTime CreationDate { get; private set; }
+    }
+}
---
+++ eventbus-messages/EventBus.Messages/Events/UserCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿
+namespace EventBus.Messages.Events
+{
+    public class UserCreatedEvent : IntegrationEvent
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
