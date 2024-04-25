f622d44a2dc5587700a51933abd433c60ac26ea3
sfgserr <dead01r@gmail.com>
2024-04-25 18:34:50 +0300

    Add controls to AccountScreen

--- choice/src/ClientApp/Screens/AccountScreen.jsx
+++ choice/src/ClientApp/Screens/AccountScreen.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import {
     View,
     Text,
-    Image
+    Image,
+    TouchableOpacity,
+    TextInput,
+    ScrollView,
+    Dimensions
 } from 'react-native';
 import * as RNFS from 'react-native-fs';
 import userStore from '../services/userStore';
+import styles from '../Styles';
 
 export default function AccountScreen({ navigation }) {
     const user = userStore.get();
 
+    const { width, height } =  Dimensions.get('screen');
+
+    const [email, setEmail] = React.useState(user.email);
+    const [name, setName] = React.useState(user.name);
+    const [surname, setSurname] = React.useState(user.surname);
+    const [phone, setPhone] = React.useState(user.phoneNumber);
+    const [address, setAddress] = React.useState(`${user.city},${user.street}`);
+
     return (
-        <View style={{flex: 1, backgroundColor: 'white'}}>
+        <ScrollView 
+            style={{flex: 1, backgroundColor: 'white'}}
+            showsVerticalScrollIndicator={false}>
             <View style={{justifyContent: 'center', paddingTop: 20}}>
                 <Text 
                     style={{
@@ -33,6 +48,139 @@ export default function AccountScreen({ navigation }) {
                         alignSelf: 'center',
                     }}/>
             </View>
-        </View>
+            <View 
+                style={{
+                    paddingTop: 20,
+                    alignSelf: 'center'
+                }}>
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: 'transparent',
+                    }}>
+                    <Text 
+                        style={{
+                            color: '#2D81E0',
+                            fontSize: 15,
+                            fontWeight: '500',
+                        }}>
+                        Изменить фото
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <View
+                style={{
+                    paddingTop: 30,
+                    paddingHorizontal: 20
+                }}>
+                <Text
+                    style={{
+                        color: '#6D7885', 
+                        fontWeight: '400', 
+                        fontSize: 14, 
+                        paddingBottom: 5
+                    }}>
+                    Имя        
+                </Text>
+                <View style={{paddingBottom: 20}}>
+                    <View style={[styles.textInput]}>
+                        <TextInput 
+                            style={[styles.textInputFont]} 
+                            value={name} 
+                            onChangeText={(text) => setName(text)}/>
+                    </View>
+                </View>
+
+                <Text
+                    style={{
+                        color: '#6D7885', 
+                        fontWeight: '400', 
+                        fontSize: 14, 
+                        paddingBottom: 5
+                    }}>
+                    Фамилия        
+                </Text>
+                <View style={{paddingBottom: 20}}>
+                    <View style={[styles.textInput]}>
+                        <TextInput 
+                            style={[styles.textInputFont]} 
+                            value={surname} 
+                            onChangeText={(text) => setSurname(text)}/>
+                    </View>
+                </View>
+                <Text
+                    style={{
+                        color: '#6D7885', 
+                        fontWeight: '400', 
+                        fontSize: 14, 
+                        paddingBottom: 5
+                    }}>
+                    E-mail        
+                </Text>
+                <View style={{paddingBottom: 20}}>
+                    <View style={[styles.textInput]}>
+                        <TextInput 
+                            style={[styles.textInputFont]} 
+                            value={email} 
+                            onChangeText={(text) => setEmail(text)}/>
+                    </View>
+                </View>
+                <Text
+                    style={{
+                        color: '#6D7885', 
+                        fontWeight: '400', 
+                        fontSize: 14, 
+                        paddingBottom: 5
+                    }}>
+                    Телефон        
+                </Text>
+                <View style={{paddingBottom: 20}}>
+                    <View style={[styles.textInput]}>
+                        <TextInput 
+                            style={[styles.textInputFont]} 
+                            value={phone} 
+                            onChangeText={(text) => setPhone(text)}/>
+                    </View>
+                </View>
+                <Text
+                    style={{
+                        color: '#6D7885', 
+                        fontWeight: '400', 
+                        fontSize: 14, 
+                        paddingBottom: 5
+                    }}>
+                    Адрес        
+                </Text>
+                <View style={{paddingBottom: 20}}>
+                    <View style={[styles.textInput, {height: height/7}]}>
+                        <TextInput 
+                            style={[styles.textInputFont]} 
+                            value={address} 
+                            onChangeText={(text) => setAddress(text)}/>
+                    </View>
+                </View>
+                <TouchableOpacity style={[styles.button, { backgroundColor: '#001C3D0D' }]}>
+                    <Text
+                        style={[styles.buttonText, { color: '#2688EB' }]}>
+                        Изменить пароль
+                    </Text>
+                </TouchableOpacity>
+                <View style={{paddingTop: 20}}>
+                    <TouchableOpacity style={[styles.button, { backgroundColor: '#001C3D0D' }]}>
+                        <Text
+                            style={[styles.buttonText, { color: '#EB2626' }]}>
+                            Выйти из аккаунта
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{paddingTop: 20}}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text
+                            style={styles.buttonText}>
+                            Сохранить изменения
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ScrollView>
     );
 }
