6b217037915e4ccc7325df04c82c732ba5cb19e8
sfgserr <dead01r@gmail.com>
2024-05-07 18:00:36 +0300

    Add CompanyRequestCreationScreen

---
+++ choice/src/ClientApp/Screens/CompanyRequestCreationScreen.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+    View
+} from 'react-native';
+
+const CompanyRequestCreationScreen = ({navigation, orderRequest}) => {
+    return (
+        <View
+            style={{flex: 1, backgroundColor: 'white'}}>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between'
+                }}>
+                
+            </View>
+        </View>
+    )
+}
