d6f009c84b0a2d93a1f07be7eba112f4b530fb38
sfgserr <dead01r@gmail.com>
2024-09-13 19:21:13 +0300

    Add MarkAsPaid command

---
+++ Backend/src/Modules/Users/Application/OrderResponses/Commands/MarkAsPaid/MarkAsPaidCommand.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.Application.Cqrs.Commands;
+
+namespace Users.Application.OrderResponses.Commands.MarkAsPaid
+{
+    public class MarkAsPaidCommand : InternalCommandBase
+    {
+        public MarkAsPaidCommand(Guid id, Guid responseId) : base(id)
+        {
+            ResponseId = responseId;
+        }
+
+        public Guid ResponseId { get; }
+    }
+}
