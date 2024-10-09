9766d4d6c52d3eed9c93aa6dd9ed354b2452f73d
sfgserr <dead01r@gmail.com>
2024-10-10 00:11:09 +0300

    Add CreateUsersReturnsOk test

--- Backend/tests/IntegrationTests/Users/Users/CreateUser/CreateUserTests.cs
+++ Backend/tests/IntegrationTests/Users/Users/CreateUser/CreateUserTests.cs
@@ -1,7 +1,34 @@
+using System.Text.Json;
+
 namespace IntegrationTests.Users.Users.CreateUser
 {
-    public class CreateUserTests
+    public class CreateUserTests : IClassFixture<ChoiceWebApplicationFactory>
     {
-        
+        private readonly ChoiceWebApplicationFactory _factory;
+
+        public CreateUserTests(ChoiceWebApplicationFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task CreateUserReturnsOk()
+        {
+            var client = _factory.CreateClient();
+
+            var result = await client.PostAsync(
+                "/api/users",
+                new StringContent(JsonSerializer.Serialize(new 
+                {
+                    Name = "string",
+                    Email = "string",
+                    Password = "string",
+                    PhoneNumber = "string",
+                    City = "string",
+                    Street = "string"
+                })));
+
+            Assert.True(result.IsSuccessStatusCode);
+        }
     }
 }
