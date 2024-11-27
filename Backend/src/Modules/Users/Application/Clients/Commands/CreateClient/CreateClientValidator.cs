842a27055782735d037d0e83cecaf3d855887793
sfgserr <dead01r@gmail.com>
2024-11-27 18:40:08 +0200

    Add Validation for email and password

--- Backend/src/Modules/Users/Application/Clients/Commands/CreateClient/CreateClientValidator.cs
+++ Backend/src/Modules/Users/Application/Clients/Commands/CreateClient/CreateClientValidator.cs
@@ -14,7 +14,10 @@ namespace Users.Application.Clients.Commands.CreateClient
             RuleFor(c => c.Password)
                 .NotEmpty()
                 .NotNull()
-                .WithMessage("Name must be provided");
+                .WithMessage("Name must be provided")
+                .MinimumLength(8)
+                .MaximumLength(16)
+                .WithMessage("Password length should in range from 8 to 16");
 
             RuleFor(c => c.City)
                 .NotEmpty()
@@ -25,6 +28,13 @@ namespace Users.Application.Clients.Commands.CreateClient
                 .NotEmpty()
                 .NotNull()
                 .WithMessage("Name must be provided");
+
+            RuleFor(c => c.Email)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Email must be provided")
+                .EmailAddress()
+                .WithMessage("Invalid email");
         }
     }
 }
