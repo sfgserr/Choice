a22730b16d0f0815ff3b7826e03a5eb5f679a856
sfgserr <dead01r@gmail.com>
2025-03-30 23:27:45 +0300

    Add location for minio console in nginx

--- App/babel.config.js
+++ App/babel.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    ['module:react-native-dotenv'],
+    ['module:react-native-dotenv', {
+      "path": ".env.production",
+    }],
     'react-native-reanimated/plugin',
   ],
 };
